6c406ba917718a752a700545885fa9bb62c1a487
pm24mcgi <pmcginn@highlinefm.com>
2022-06-26 00:26:36 -0400

    profile form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .booking_form import BookingForm
 from .review_form import ReviewForm
 from .spot_form import SpotForm
+from .profile_form import ProfileForm
