be852066d8ffa30d50da9c20bd9acff200354f73
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-06-26 18:59:12 -0700

    Creation of amenity migration

--- app/models/spot_amenity_join.py
+++ app/models/spot_amenity_join.py
@@ -3,6 +3,5 @@ from .db import db
 class SpotAmenityJoin(db.Model):
   __tablename__ = "spot_amenity_join"
 
-  id = db.Column(db.Integer, primary_key=True)
-  spot_id = db.Column(db.Integer, db.ForeignKey('spots.id'), nullable=False)
-  spot_details_id = db.Column(db.Integer, db.ForeignKey('spots_details.id'), nullable=False)
+  spot_id = db.Column(db.Integer, db.ForeignKey('spots.id'), nullable=False, primary_key=True)
+  amenity_id = db.Column(db.Integer, db.ForeignKey('amenities.id'), nullable=False, primary_key=True)
