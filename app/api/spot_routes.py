78645ca3ddee693f80107448f1d4a94a61bc124d
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-06-27 09:58:15 -0700

    Creation of spots route and some changes on amenties join

--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import Spot, db
 from app.forms import spot_form
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import login_required
+
+from ..models.spot import Spot
 
 spot_routes = Blueprint('spots', __name__)
 
 
 # Route provides all avaialble spots
-# @spot_routes.route('/')
-# @login_required
-# def all_spots():
+@spot_routes.route('/')
+@login_required
+def all_spots():
+  spots = Spot.query.all()
+  print(spots)
+  return {spot.id: spot.to_dict() for spot in spots}
