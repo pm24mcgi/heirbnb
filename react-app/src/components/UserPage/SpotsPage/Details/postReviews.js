050c4af64d097fa874e6d51d1208e287c8a7b247
pm24mcgi <pmcginn@highlinefm.com>
2022-06-28 18:40:28 -0400

    rating system

--- react-app/src/components/UserPage/SpotsPage/Details/postReviews.js
+++ react-app/src/components/UserPage/SpotsPage/Details/postReviews.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { postReview } from '../../../../store/reviews'
+import { Rating } from 'react-simple-star-rating'
 
 // should not be allowed to review property more than once?
 // set review value defaulted to false, , onsubmit true, if conditional to submit
@@ -10,13 +11,19 @@ const ReviewForm = () => {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const [rating, setRating] = useState(0);
+  const [adjRating, setAdjRating] = useState(0);
   const [review, setReview] = useState('');
+
+  const handleRating = (rate) => {
+    setAdjRating(rate)
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const spot_id = 1;
+    const rating = adjRating/20
+    console.log(rating)
 
     const payload = {
       spot_id,
@@ -33,12 +40,14 @@ const ReviewForm = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Rating
-          <input
+          <Rating onClick={handleRating} ratingValue={adjRating} fillColor={'rgb(225,20,20)'} size={20} allowHalfIcon={true} showTooltip={true} tooltipDefaultText={'Your Rating'}/>
+          {/* <div>{rating/20}</div> */}
+          {/* <input
             type="number"
             value={rating}
             onChange={(e) => setRating(e.target.value)}
             required
-          />
+          /> */}
         </label>
         <label>
           Deatils
