09fe32a7e72c05ded4ba37721bdc7dc871763089
pm24mcgi <pmcginn@highlinefm.com>
2022-07-01 13:58:17 -0400

    edit and delete functionality

--- react-app/src/components/Reviews/getReviews.js
+++ react-app/src/components/Reviews/getReviews.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Rating } from 'react-simple-star-rating'
@@ -7,20 +8,24 @@ import EditReview from "./editReviews";
 const GetReviews = () => {
   const reviews = Object.values(useSelector(state => state.review))
   const user = useSelector(state => state.session.user);
-  console.log(user.id)
   const {spotId} = useParams();
+
+  const [editOpen, setEditOpen] = useState(false)
 
   return (
     <div>
       <h2>Reviews:</h2>
       {reviews.map((review) => {
         if (spotId == review.spotId) {
-          console.log(review)
           return (
             <div key={review.id}>{review.review}
               <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
               <div>
-                {user.id === review.userId ? <EditReview reviewProp={review}/> : null}
+                {user.id === review.userId ?
+                <div>
+                  <button onClick={() => setEditOpen(!editOpen)}>Edit Review</button>
+                  {editOpen && <EditReview setEditOpen={setEditOpen} reviewProp={review}/>}
+                </div> : null}
               </div>
             </div>
           )
