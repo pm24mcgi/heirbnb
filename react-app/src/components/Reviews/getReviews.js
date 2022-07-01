ad71ab969079e712de3cf6d2eb5914284f5e99bf
pm24mcgi <pmcginn@highlinefm.com>
2022-07-01 12:33:00 -0400

    get route for specific spot update

--- react-app/src/components/Reviews/getReviews.js
+++ react-app/src/components/Reviews/getReviews.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { Rating } from 'react-simple-star-rating'
 import EditReview from "./editReviews";
 
 const GetReviews = () => {
   const reviews = Object.values(useSelector(state => state.review))
+  const {spotId} = useParams();
+  const spot = useSelector(state => state.spot[spotId])
+  const revArr = spot.reviews
 
   if (!reviews) {
     return(
@@ -15,17 +19,30 @@ const GetReviews = () => {
       <div>
         <h2>Reviews:</h2>
         {reviews.map((review) => {
-          return (
-            <div key={review.id}>{review.review}
-              <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
-              <EditReview reviewProp={review}/>
-            </div>
-          )
+          if (spotId == review.spotId) {
+            return (
+              <div key={review.id}>{review.review}
+                <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
+                <EditReview reviewProp={review}/>
+              </div>
+            )
+          }
         })}
+        {revArr.length === 0 ? <div>This property has not yet been reviewed</div> : null}
       </div>
     )
   }
 };
 //edit
+
+
+          // if (spotId === review.spotId) {
+          //   return (
+          //     <div key={review.id}>{review.review}
+          //       <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
+          //       <EditReview reviewProp={review}/>
+          //     </div>
+          //   )
+          // }
 
 export default GetReviews;
