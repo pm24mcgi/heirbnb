329fb5da163d3dabcadacf32012aa9df126d61b7
pm24mcgi <pmcginn@highlinefm.com>
2022-07-01 13:04:40 -0400

    edit only your own reviews

--- react-app/src/components/Reviews/getReviews.js
+++ react-app/src/components/Reviews/getReviews.js
@@ -6,31 +6,29 @@ import EditReview from "./editReviews";
 
 const GetReviews = () => {
   const reviews = Object.values(useSelector(state => state.review))
+  const user = useSelector(state => state.session.user);
+  console.log(user.id)
   const {spotId} = useParams();
   const spot = useSelector(state => state.spot[spotId])
-  const revArr = spot.reviews
 
-  if (!reviews) {
-    return(
-      <div>This property has not yet been reviewed</div>
-    )
-  } else {
-    return (
-      <div>
-        <h2>Reviews:</h2>
-        {reviews.map((review) => {
-          if (spotId == review.spotId) {
-            return (
-              <div key={review.id}>{review.review}
-                <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
-                <EditReview reviewProp={review}/>
+  return (
+    <div>
+      <h2>Reviews:</h2>
+      {reviews.map((review) => {
+        if (spotId == review.spotId) {
+          console.log(review)
+          return (
+            <div key={review.id}>{review.review}
+              <Rating ratingValue={review.rating*20} fillColor={'rgb(225,20,20)'} readonly={true} size={20} />
+              <div>
+                {user.id === review.userId ? <EditReview reviewProp={review}/> : null}
               </div>
-            )
-          }
-        })}
-      </div>
-    )
-  }
+            </div>
+          )
+        }
+      })}
+    </div>
+  )
 };
 
 export default GetReviews;
