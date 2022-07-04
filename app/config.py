04c4039977f7120e498f1004806c1c9f24bfef6c
pm24mcgi <pmcginn@highlinefm.com>
2022-07-04 17:06:50 -0400

    map removal

--- app/config.py
+++ app/config.py
@@ -11,4 +11,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
-    googleAPIKey = os.environ.get('GOOGLE_MAPS_API_KEY')
