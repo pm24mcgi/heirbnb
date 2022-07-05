2dcec4853ba8a4d66159cabf7cad777921eab02a
Osvaldo Salomon <osvaldo.salo21@gmail.com>
2022-07-05 00:51:55 -0400

    Bookings confirmed style

--- react-app/src/components/UserPage/BookingsPage/BookingConfirmation.js
+++ react-app/src/components/UserPage/BookingsPage/BookingConfirmation.js
@@ -1,42 +1,49 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { format } from "date-fns";
-import './BookingConfirmation.css';
 
 const BookingConfirmation = () => {
-    const { bookingId } = useParams();
-    const booking = useSelector(state => state.booking[bookingId]);
-    const spot = useSelector(state => state.spot[booking?.spot_id])
-    const user = useSelector(state => state.session.user);
+	const { bookingId } = useParams();
+	const booking = useSelector((state) => state.booking[bookingId]);
+	const spot = useSelector((state) => state.spot[booking?.spot_id]);
+	const user = useSelector((state) => state.session.user);
 
-    const start = new Date(booking?.start_date)
-    const startNew = start.getTime()
-    const end = new Date(booking?.end_date)
-    const endNew = end.getTime()
-    const timeSpan = endNew - startNew;
-    const numOfDays = timeSpan / (1000 * 60 * 60 * 24)
+	const start = new Date(booking?.start_date);
+	const startNew = start.getTime();
+	const end = new Date(booking?.end_date);
+	const endNew = end.getTime();
+	const timeSpan = endNew - startNew;
+	const numOfDays = timeSpan / (1000 * 60 * 60 * 24);
 
-    const checkIn = format(start, "MMMM do, yyyy");
-    const checkOut = format(end, "MMMM do, yyyy");
+	const checkIn = format(start, "MMMM do, yyyy");
+	const checkOut = format(end, "MMMM do, yyyy");
 
-    return (
-        <div className='booking_container'>
-            <h1 className='booking-blurb'>
-                Your reservation was a success, {user?.username}!
-                <br />
-                We hope you enjoy your stay!
-            </h1>
+	return (
+		<div className="bookingContainer">
+			<h1 className="booking-blurb">
+				Your reservation was a success, {user?.username}!
+				<br />
+				We hope you enjoy your stay!
+			</h1>
 
-            <img className='spot-image' src={spot.images[0].url} alt={`${spot.images[0].id}`} />
-            <div className='booking-details'>
-                <h3>Check-in: {checkIn}</h3>
-                <h3>Check-out: {checkOut}</h3>
-                <h3>Address: {spot?.address}</h3>
-                <h3>Total: ${numOfDays * spot?.price_per_day}</h3>
-            </div>
-        </div>
-    )
-}
+			<img
+				className="spot-image"
+				src={spot.images[0].url}
+				alt={`${spot.images[0].id}`}
+			/>
+			<div className="booking-details">
+				<div className='booking-details-dates'>
+					<h3>Check-in: {checkIn}</h3> 
+					<h3>Check-out: {checkOut}</h3>
+				</div>
+				<h3>
+					Address: #{spot?.address}, {spot?.city}, {spot?.state}
+				</h3>
+				<h3>Total: ${numOfDays * spot?.price_per_day}</h3>
+			</div>
+		</div>
+	);
+};
 
-export default BookingConfirmation
+export default BookingConfirmation;
